9802fa5ad3d98168efdb033fc8e554c783ae24e6
hyeonsu_ <117705439+agness4011@users.noreply.github.com>
2026-03-30 20:52:48 +0900

    Update heading text in App component

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>깃허브 고고</h1>
+          <h1>깃허브 고,고고</h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
